b5fca8afc1a3aafe0497f95e29b153ddd251b7c6
Usman Mughal <35332218+usmanmughalji@users.noreply.github.com>
2021-01-02 19:55:57 +0500

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -14,12 +14,14 @@ sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
+sudo apt install --assume-yes task-xfce-desktop
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo adduser CollabRDP chrome-remote-desktop
+sudo adduser -a -G CollabRDP chrome-remote-desktop
+logout
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
